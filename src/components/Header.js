ab87a9477285de1bdf85b76f04dd1bb442fb62d2
princek112002 <princekreme@gmail.com>
2023-02-26 22:25:02 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <header className='header-upper py-3'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-2'>
+            <h1 className='text-white'>
+              <Link className='text-white'>MySchool</Link>
+            </h1>
+          </div>
+          <div className='col-6'></div>
+        </div>
+      </div>
+    </header>
+    </>
   );
 };
 
