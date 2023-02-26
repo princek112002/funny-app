2e092c994c1d369bb9d6f0fd4d9a2702b2664f3a
yannemmanuel <yannandrekoffi@gmail.com>
2023-02-26 15:55:58 +0000

    Update Emploi_du_temps.js

--- src/pages/Emploi_du_temps.js
+++ src/pages/Emploi_du_temps.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Emploi_du_temps = () => {
   return (
     <div className='MyEmp'>
-      <p> c est ta partie</p>
+      <p> c'est ta partie</p>
     </div>
   );
 };
