c674c3bd452b6068937eaa948c6db3578953b3fd
yannemmanuel <yannandrekoffi@gmail.com>
2023-02-26 14:53:42 +0000

    Create Emploi_du_temps.js

---
+++ src/pages/Emploi_du_temps.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Emploi_du_temps = () => {
+  return (
+    <div className='MyEmp'>
+      <p> c est ta partie</p>
+    </div>
+  );
+};
+
+export default Emploi_du_temps;
