f72fac62dde0a86bfeb6f533369a72699f68d15a
princek112002 <princekreme@gmail.com>
2023-02-27 02:17:32 +0000

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
 import React from 'react';
-
+import BlogCard from '../Components/BlogCard';
+import ClubsCard from '../Components/ClubsCard';
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className='blog-wrapper py-5 home-wrapper-2'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='section-heading'>Nos Clubs</div>
+          </div>
+          <ClubsCard/>
+          <ClubsCard/>
+          <ClubsCard/>
+          <ClubsCard/>
+        </div>
+      </div>
+    </section>
+
+    <section className='blog-wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='section-heading'>Amusez Vous en apprenant</div>
+            </div>
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
+            <BlogCard/>
+          </div>
+        </div>
+    </section>
+    </>
   );
 };
 
