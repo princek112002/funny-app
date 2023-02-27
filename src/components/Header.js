c386f328dd189edbb6a17e912a6e7521ea943964
princek112002 <princekreme@gmail.com>
2023-02-27 01:13:33 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,26 @@ import { NavLink, Link } from 'react-router-dom';
 const Header = () => {
   return (
     <>
-    <header className='header-upper py-3'>
+    <header className='header-upper py-1'>
       <div className='container-xxl'>
         <div className='row'>
-          <div className='col-2'>
-            <h1 className='text-white'>
-              <Link className='text-white'>MySchool</Link>
-            </h1>
+          <div className='col-2 d-flex align-items-center'>
+            <div className='logo'>
+              <h1 className='head-title'>
+                MySchool
+              </h1>
+            </div>
+            <div className='menu-links'>
+                <div className='d-flex align-items-center gap-30'>
+                  <Link>
+                    <img src='images/homepage.png' alt='homeicons' />
+                  </Link>
+                  <NavLink className='links' to='/'>News</NavLink>
+                  <NavLink className='links' to='/'>Salle</NavLink>
+                  <NavLink className='links' to='/'>Emploi_du_temps</NavLink>
+                </div>
+              </div>
           </div>
-          <div className='col-6'></div>
         </div>
       </div>
     </header>
