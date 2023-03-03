a6681d9434c9166a8b2a5fa8bda218b7e2b7aeac
yannemmanuel <yannandrekoffi@gmail.com>
2023-03-03 19:40:09 +0000

    Update News.js

--- src/pages/News.js
+++ src/pages/News.js
@@ -4,7 +4,16 @@ const News = () => {
   return (
     <div>
       <p>C'est Yann qui réalise cette page</p>
+      <div id="app-layout-body" class="container-fluid body-content">
+    <div class="row">
+        <div id="div-register" class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+            center with offset
+        </div>
     </div>
+</div>
+
+    </div>
+    
   
   );
 };
